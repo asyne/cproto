f8d65db2e12b61e387f5b85676b78bbb893f32ac
Evan <evan.inout@gmail.com>
2019-09-12 22:23:16 +0300

    Correct PyPi project description

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except:
 
 setup(
     name='cproto',
-    version='0.7.0',
+    version='0.7.1',
     description='Chrome Debugging Protocol client',
     long_description=long_description,
     url='https://github.com/asyne/cproto',
